f7bb9f6b68ce4cbe35a5f0ee5d06346f1d8f7d4d
RanjanaBhat <ranjana.bhat@anchanto.com>
2016-01-09 17:36:12 +0530

    Blog Uploaded

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App2016::Application.config.secret_token = 'aad7c323e0c508070ca71e16f2313f20b48717b828a0deb9699f31b63e4d6cc4b7ef6d5cead0c2bec89cacf4b688ae769ca6f99443fd4e855f99c85d3efe5630'
